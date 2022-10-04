efd59a9731c68d5771b40b6f1500d3f7c043904c
SungYu93 <tjddb111@naver.com>
2022-10-04 12:45:00 +0900

    어노테이션 정의 완료

--- src/main/java/site/metacoding/chatt/WebSocketChatt.java
+++ src/main/java/site/metacoding/chatt/WebSocketChatt.java
@@ -1,5 +1,35 @@
 package site.metacoding.chatt;
 
-public class WebSocketChatt {
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@ServerEndpoint(value = "/chatt")
+public class WebSocketChatt {
+	private static Set<Session> clients = 
+			Collections.synchronizedSet(new HashSet<Session>());
+	
+	@OnMessage // 메시지가 수신되었을 때
+	public void onMessage(String msg, Session session) throws Exception{
+	}
+	
+	@OnOpen // ㅋ클라이언트가 접속할 때 발생하는 이벤트
+	public void onOpen(Session s) {
+		
+	}
+	
+	@OnClose //클라이언트가 브라우저를 끄거나 다른 경로로 이동할 때
+	public void onClos(Session s) {
+		
+	}
+	
 }
