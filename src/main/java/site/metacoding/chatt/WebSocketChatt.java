844e092e833f684c5cb2c9420780e1dd701c9c0e
SungYu93 <tjddb111@naver.com>
2022-10-04 14:31:28 +0900

    WebSocketChatt 패키지 완료

--- src/main/java/site/metacoding/chatt/WebSocketChatt.java
+++ src/main/java/site/metacoding/chatt/WebSocketChatt.java
@@ -18,18 +18,32 @@ public class WebSocketChatt {
 	private static Set<Session> clients = 
 			Collections.synchronizedSet(new HashSet<Session>());
 	
-	@OnMessage // 메시지가 수신되었을 때
-	public void onMessage(String msg, Session session) throws Exception{
-	}
-	
-	@OnOpen // ㅋ클라이언트가 접속할 때 발생하는 이벤트
+	@OnOpen // 클라이언트가 접속할 때 발생하는 이벤트
 	public void onOpen(Session s) {
+		System.out.println("open session: " + s.toString());
+		if(!clients.contains(s)) {
+			clients.add(s);
+			System.out.println("session open: " + s);
+		}else {
+			System.out.println("이미 연결된 session 입니다.");
+		}
 		
 	}
+	
+	@OnMessage // 메시지가 수신되었을 때
+	public void onMessage(String msg, Session session) throws Exception{
+		System.out.println("receive message: " + msg);
+		for(Session s : clients) {
+			System.out.println("send data: " + msg);
+			s.getBasicRemote().sendText(msg);
+		}
+	}
+	
 	
 	@OnClose //클라이언트가 브라우저를 끄거나 다른 경로로 이동할 때
 	public void onClos(Session s) {
-		
+		System.out.println("session close: " + s);
+		clients.remove(s);
 	}
 	
 }
