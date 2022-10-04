62da0d96510032fa8be4cbad3d77aa4f7e805711
SungYu93 <tjddb111@naver.com>
2022-10-04 12:41:40 +0900

    웹소켓 초기 세팅 완료

---
+++ src/main/java/site/metacoding/chatt/WebSocketChatt.java
@@ -0,0 +1,5 @@
+package site.metacoding.chatt;
+
+public class WebSocketChatt {
+
+}
---
+++ src/test/java/site/metacoding/SpringBootWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
